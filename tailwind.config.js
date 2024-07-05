4914f1d91b16551cd1f41a1d7a8097fb5ee6dbe6
raphael <raphael.pellet@protonmail.com>
2024-07-05 11:04:45 +0200

    :gear: add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: {
+    relative: true,
+    files: ["./app/src/**/*.rs"],
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// Docs & references: https://tailwindcss.com/docs/theme
