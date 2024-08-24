a317768e2fabc7a8625cacd2cf2255ab160a9bac
raphael <raphael.pellet@protonmail.com>
2024-08-24 12:39:57 +0200

    :cookie: add liw-web for cookies & sessions management

---
+++ libs/lib-web/src/cookies.rs
@@ -0,0 +1,40 @@
+use crate::Result;
+use lib_core::model::session::{SessionForAuth, SessionType};
+use lib_utils::time::parse_utc;
+use tower_cookies::cookie::{CookieBuilder, SameSite};
+use tower_cookies::{Cookie, Cookies};
+
+pub const SESSION_TOKEN: &str = "session-token";
+
+pub fn set_session_cookie(cookies: &Cookies, session_auth: SessionForAuth) -> Result<()> {
+    let SessionForAuth {
+        token,
+        session_type,
+        expiration,
+    } = session_auth;
+
+    let mut cookie = CookieBuilder::new(SESSION_TOKEN, token)
+        .path("/")
+        .http_only(true)
+        .secure(true)
+        .same_site(SameSite::Strict)
+        .build();
+
+    // Add expiration only in case of a non privileged session
+    if !SessionType::from(session_type).is_privileged() {
+        cookie.set_expires(parse_utc(&expiration)?);
+    }
+
+    cookies.add(cookie);
+
+    Ok(())
+}
+
+pub fn remove_session_cookie(cookies: &Cookies) -> Result<()> {
+    let mut cookie = Cookie::from(SESSION_TOKEN);
+    cookie.set_path("/");
+
+    cookies.remove(cookie);
+
+    Ok(())
+}
